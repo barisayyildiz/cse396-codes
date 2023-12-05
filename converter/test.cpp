7a77bd03594effb027643baa30e32fd4abfef255
barisayyildiz <99ayyldzbaris99@gmail.com>
2023-12-05 21:14:48 +0300

    feat: resimler raspberry den desktop a gönderilebiliyor

--- converter/test.cpp
+++ converter/test.cpp
@@ -92,8 +92,9 @@ int main() {
         // cap >> img;
         // cap.release();
         cv::Mat img;
+        cv::Mat cropped;
 
-        std::string save_path = "/home/barisayyildiz/cse396-codes/converter/imgs_db/original/" + std::to_string(counter) + ".jpg";
+        std::string save_path = "imgs_db/original/" + std::to_string(counter) + ".jpg";
         img = cv::imread(save_path);
 
         // save_path = "imgs/original/" + std::to_string(counter) + ".jpg";
@@ -105,13 +106,13 @@ int main() {
         pts[2] = { 733.0, 634.0 };
         pts[3] = { 277.0, 634.0 };
 
-        img = fourPointTransform(img, std::vector<cv::Point2f>(pts, pts + 4));
+        cropped = fourPointTransform(img, std::vector<cv::Point2f>(pts, pts + 4));
         save_path = "imgs_db/four_points/" + std::to_string(counter) + ".jpg";
-        cv::imwrite(save_path, img);
+        cv::imwrite(save_path, cropped);
 
-        int h = img.rows;
-        int w = img.cols;
-        Mat backG = Mat::zeros(h, w, CV_32F);
+        int h = cropped.rows;
+        int w = cropped.cols;
+        Mat backG = Mat::zeros(h, w, CV_8U);
         int bottomR = 0;
         int topR = 0;
         vector<Vertex> tempV;
@@ -122,13 +123,13 @@ int main() {
             int max = -1;
             int cIndex = -1;
             for(int j=0; j<w; j++) {
-                int current = img.at<cv::Vec3b>(i, j)[2];
+                int current = cropped.at<cv::Vec3b>(i, j)[2];
                 if(current > max) {
                     cIndex = j;
                     max = current;
                 }
             }
-            if(img.at<cv::Vec3b>(i, cIndex)[2] > 25) {
+            if(cropped.at<cv::Vec3b>(i, cIndex)[2] > 25) {
                 backG.at<uchar>(i, cIndex) = 1;
                 bottomR = i;
                 if(topR == 0) {
@@ -137,7 +138,7 @@ int main() {
             }
         }
         save_path = "imgs_db/red_line/" + std::to_string(counter) + ".jpg";
-        cv::imwrite(save_path, backG);
+        cv::imwrite(save_path, backG*255);
 
         int centerC = 275;
         for (int r = 0; r < h; r++) {
@@ -192,13 +193,44 @@ int main() {
         vector<Vertex> vertices = meshPoints.back();
         uint numOfScannedPoints = vertices.size();
 
-        for(int i=0; i<numOfScannedPoints; i++) {
-            memset(buffer, '\0', BUFFER_SIZE);
-            double x = vertices.at(i).x, y = vertices.at(i).y, z = vertices.at(i).z;
+        if(FEATURE_COMMUNICATION) {
+            for(int i=0; i<numOfScannedPoints; i++) {
+                memset(buffer, '\0', BUFFER_SIZE);
+                double x = vertices.at(i).x, y = vertices.at(i).y, z = vertices.at(i).z;
 
-            sprintf(buffer, "%lf %lf %lf", x, y, z);
-            send(dataSocket, buffer, sizeof(buffer), 0);
-            recv(dataSocket, buffer, sizeof(buffer), 0);
+                 sprintf(buffer, "%lf %lf %lf", x, y, z);
+                send(dataSocket, buffer, sizeof(buffer), 0);
+                recv(dataSocket, buffer, sizeof(buffer), 0);
+            }
+
+            // send image size
+            std::vector<uchar> imageBuffer;
+            imencode(".jpg", img, imageBuffer);
+            int imgSize = imageBuffer.size();
+            send(dataSocket, &imgSize, sizeof(int), 0);
+            
+            // send image
+            int chunkSize = 1024; // Choose an appropriate chunk size
+            for (int i = 0; i <imgSize; i += chunkSize) {
+                int remaining = std::min(chunkSize, imgSize - i);
+                send(dataSocket, imageBuffer.data() + i, remaining, 0);
+                recv(dataSocket, buffer, BUFFER_SIZE, 0);
+            }
+
+            // send image size
+            imageBuffer;
+            imencode(".jpg", backG*255, imageBuffer);
+            imgSize = imageBuffer.size();
+            send(dataSocket, &imgSize, sizeof(int), 0);
+            
+            // send image
+            chunkSize = 1024; // Choose an appropriate chunk size
+            for (int i = 0; i <imgSize; i += chunkSize) {
+                int remaining = std::min(chunkSize, imgSize - i);
+                send(dataSocket, imageBuffer.data() + i, remaining, 0);
+                recv(dataSocket, buffer, BUFFER_SIZE, 0);
+            }
+
         }
 
         // move(true, STEP_PER_MOVEMENT);
